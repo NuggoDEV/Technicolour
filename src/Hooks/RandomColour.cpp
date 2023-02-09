7aa60905e96eb894fbc6a117e72c7c5375e1cf34
NuggoDEV <NuggoDEV@gmail.com>
2023-02-09 18:30:38 +1030

    Use config values

---
+++ src/Hooks/RandomColour.cpp
@@ -0,0 +1,87 @@
+#include "Hooks.hpp"
+#include "ModConfig.hpp"
+
+#include "GlobalNamespace/NoteController.hpp"
+#include "GlobalNamespace/NoteData.hpp"
+#include "GlobalNamespace/ColorType.hpp"
+
+#include "GlobalNamespace/BombNoteController.hpp"
+
+#include "GlobalNamespace/ObstacleController.hpp"
+#include "GlobalNamespace/ObstacleData.hpp"
+#include "GlobalNamespace/ColorType.hpp"
+
+#include "GlobalNamespace/BasicBeatmapEventData.hpp"
+#include "GlobalNamespace/LightSwitchEventEffect.hpp"
+#include "GlobalNamespace/BasicBeatmapEventData.hpp"
+using namespace GlobalNamespace;
+
+#include "UnityEngine/Color.hpp"
+#include "UnityEngine/Vector3.hpp"
+using namespace UnityEngine;
+
+#include <iostream>
+#include <cstdlib>
+#include <random>
+
+#include "sombrero/shared/FastColor.hpp"
+using namespace Sombrero;
+
+#include "chroma/shared/BombAPI.hpp"
+#include "chroma/shared/NoteAPI.hpp"
+#include "chroma/shared/ObstacleAPI.hpp"
+#include "chroma/shared/LightAPI.hpp"
+
+FastColor RandomColourGen()
+{
+  static std::mt19937 gen(std::random_device{}());
+  static std::uniform_real_distribution<> dis(0.0, 1.0);
+  float hue = dis(gen);
+  float saturation = 1.0f;
+  float value = 1.0f;
+
+  FastColor colour = FastColor::HSVToRGB(hue, saturation, value);
+  return colour;
+}
+
+MAKE_AUTO_HOOK_MATCH(NoteController_Init, &NoteController::Init, void, NoteController *self, NoteData *noteData, float worldRotation, Vector3 moveStartPos, Vector3 moveEndPos,
+  Vector3 jumpEndPos, float moveDuration, float jumpDuration, float jumpGravity, float endRotation, float uniformScale, bool rotateTowardsPlayer, bool useRandomRotation)
+{
+  NoteController_Init(self, noteData, worldRotation, moveStartPos, moveEndPos, jumpEndPos, moveDuration,
+    jumpDuration, jumpGravity, endRotation, uniformScale, rotateTowardsPlayer, useRandomRotation);
+
+  if (getModConfig().ModToggle.GetValue() && getModConfig().TechniNotes.GetValue() == "True Random" && noteData->colorType == ColorType::ColorA)
+    Chroma::NoteAPI::setInitialNoteControllerColorSafe(self, RandomColourGen());
+
+  else if (getModConfig().ModToggle.GetValue() && getModConfig().TechniNotes.GetValue() == "True Random" && noteData->colorType == ColorType::ColorB)
+    Chroma::NoteAPI::setInitialNoteControllerColorSafe(self, RandomColourGen());
+}
+
+MAKE_AUTO_HOOK_MATCH(BombController_Init, &BombNoteController::Init, void, BombNoteController *self, NoteData *noteData, float worldRotation,
+                     Vector3 moveStartPos, Vector3 moveEndPos, Vector3 jumpEndPos, float moveDuration, float jumpDuration, float jumpGravity)
+{
+  BombController_Init(self, noteData, worldRotation, moveStartPos, moveEndPos, jumpEndPos, moveDuration, jumpDuration, jumpGravity);
+
+  if (getModConfig().ModToggle.GetValue() && getModConfig().TechniBombs.GetValue() == "True Random")
+    Chroma::BombAPI::setBombColorSafe(self, RandomColourGen());
+}
+
+MAKE_AUTO_HOOK_MATCH(ObstacleController_Init, &ObstacleController::Init, void, ObstacleController *self, ObstacleData *obstacleData, float worldRotation,
+                     Vector3 startPos, Vector3 midPos, Vector3 endPos, float move1Duration, float move2Duration, float singleLineWidth, float height)
+{
+  ObstacleController_Init(self, obstacleData, worldRotation, startPos, midPos, endPos, move1Duration, move2Duration, singleLineWidth, height);
+
+  if (getModConfig().ModToggle.GetValue() && getModConfig().TechniWalls.GetValue() == "True Random")
+    Chroma::ObstacleAPI::setObstacleColorSafe(self, RandomColourGen());
+}
+
+MAKE_AUTO_HOOK_MATCH(sdfjhkgf, &LightSwitchEventEffect::HandleColorChangeBeatmapEvent, void, LightSwitchEventEffect *self, BasicBeatmapEventData *basicBeatmapEventData)
+{
+  sdfjhkgf(self, basicBeatmapEventData);
+
+  if (getModConfig().ModToggle.GetValue() && getModConfig().TechniLights.GetValue() == "True Random")
+  {
+    // Chroma::LightAPI::setAllLightingColorsSafe(true, Chroma::LightAPI::LSEData{RandomColourGen(), RandomColourGen(), RandomColourGen(), RandomColourGen()});
+    Chroma::LightAPI::setLightColorSafe(self->event, false, Chroma::LightAPI::LSEData{Sombrero::FastColor(1, 0, 0, 1), Sombrero::FastColor(1, 1, 0, 1), Sombrero::FastColor(1, 0, 1, 1), Sombrero::FastColor(0, 0, 0, 1)});
+  }
+}
---
+++ src/Hooks/WarmCoolColour.cpp
@@ -0,0 +1,112 @@
+#include "main.hpp"
+#include "Hooks.hpp"
+#include "ModConfig.hpp"
+
+#include "GlobalNamespace/GameplayCoreInstaller.hpp"
+#include "GlobalNamespace/AudioTimeSyncController.hpp"
+using namespace GlobalNamespace;
+
+#include "sombrero/shared/FastColor.hpp"
+using namespace Sombrero;
+
+#include "chroma/shared/SaberAPI.hpp"
+#include "chroma/shared/BombAPI.hpp"
+#include "chroma/shared/NoteAPI.hpp"
+#include "chroma/shared/ObstacleAPI.hpp"
+#include "chroma/shared/LightAPI.hpp"
+
+#include <array>
+#include <unordered_map>
+
+float i;
+float ii;
+
+const int NUM_STEPS = 256;
+std::array<FastColor, NUM_STEPS> WarmColours;
+std::array<FastColor, NUM_STEPS> CoolColours;
+std::unordered_map<int, FastColor> WarmColourCache;
+std::unordered_map<int, FastColor> CoolColourCache;
+
+FastColor getWarmColour(int index)
+{
+  auto cachedResult = WarmColourCache.find(index);
+  if (cachedResult != WarmColourCache.end())
+  {
+    return cachedResult->second;
+  }
+
+  float t = (float)index / NUM_STEPS;
+  FastColor colour = FastColor(1.0f, 1.0f - t * 0.5f, 0.0f, t);
+  WarmColourCache[index] = colour;
+  return colour;
+}
+
+FastColor getCoolColour(int index)
+{
+  auto cachedResult = CoolColourCache.find(index);
+  if (cachedResult != CoolColourCache.end())
+  {
+    return cachedResult->second;
+  }
+
+  float t = (float)index / NUM_STEPS;
+  FastColor colour = FastColor(0.0f, 1.0f - t, t * 0.5f, 1.0f);
+  CoolColourCache[index] = colour;
+  return colour;
+}
+
+void generateColours()
+{
+  for (int i = 0; i < NUM_STEPS; i++)
+  {
+    WarmColours[i] = getWarmColour(i);
+    CoolColours[i] = getCoolColour(i);
+  }
+}
+
+
+bool WarmToggle = true, CoolToggle = true;
+
+MAKE_AUTO_HOOK_MATCH(WC_GameplayCoreInstaller_InstallBindings, &GameplayCoreInstaller::InstallBindings, void, GameplayCoreInstaller *self)
+{
+  WC_GameplayCoreInstaller_InstallBindings(self);
+  generateColours();
+
+  i = getModConfig().LeftWarmOffset.GetValue();
+  ii = getModConfig().RightCoolOffset.GetValue();
+}
+
+MAKE_AUTO_HOOK_MATCH(WC_AudioTimeSyncController_Update, &GlobalNamespace::AudioTimeSyncController::Update, void, GlobalNamespace::AudioTimeSyncController *self)
+{
+  WC_AudioTimeSyncController_Update(self);
+
+  if (getModConfig().ModToggle.GetValue() && getModConfig().TechniNotes.GetValue() == "Warm/Cold")
+  {
+    FastColor LeftColour = getWarmColour(i);
+    FastColor RightColour = getCoolColour(ii);
+
+    Chroma::NoteAPI::setGlobalNoteColorSafe(LeftColour, RightColour);
+    Chroma::SaberAPI::setGlobalSaberColorSafe(SaberType::SaberA, LeftColour);
+    Chroma::SaberAPI::setGlobalSaberColorSafe(SaberType::SaberB, RightColour);
+
+    if (WarmToggle)
+      i++;
+    else if (!WarmToggle)
+      i--;
+
+    if (CoolToggle)
+      ii++;
+    else if (!CoolToggle)
+      ii--;
+
+    if (i >= 254)
+      WarmToggle = false;
+    else if (i <= 1)
+      WarmToggle = true;
+
+    if (ii >= 254)
+      CoolToggle = false;
+    else if (i <= 1)
+      CoolToggle = true;
+  }
+}
