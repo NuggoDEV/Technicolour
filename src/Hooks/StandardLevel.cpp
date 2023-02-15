875dc8d2e32eaa7f1dd398f436d8cb12c8eea27e
NuggoDEV <NuggoDEV@gmail.com>
2023-02-15 19:15:47 +1030

    Add checks for chroma required maps

--- src/Hooks/StandardLevel.cpp
+++ src/Hooks/StandardLevel.cpp
@@ -14,17 +14,28 @@ using namespace GlobalNamespace;
 #include <vector>
 #include <string>
 
+static inline const std::string ChromaReq = "Chroma";
+
 MAKE_AUTO_HOOK_MATCH(Level, &StandardLevelDetailView::RefreshContent, void, StandardLevelDetailView *self)
 {
     Level(self);
+    
+    // Used code from NE to instead check for Chroma
     PinkCore::API::GetFoundRequirementCallbackSafe() += [](const std::vector<std::string>& requirements) 
     {
-        bool chromaReq = std::any_of(requirements.begin(), requirements.end(), [](auto const& s) { return s == "chroma"; });
+        bool chromaReq = std::any_of(requirements.begin(), requirements.end(), [](auto const& s) { return s == ChromaReq; });
 
         if (chromaReq)
+        {
             Chroma::CoreAPI::removeForceEnableChromaHooks(modInf());
+            getModConfig().ForceDisableTechnicolour.SetValue(true);
+        }
         else 
+        {
             Chroma::CoreAPI::addForceEnableChromaHooks(modInf());
+            getModConfig().ForceDisableTechnicolour.SetValue(false);
+        }
     };
-    
+
+    getLogger().info(&"Technicolour forced to: " [ getModConfig().ForceDisableTechnicolour.GetValue() ]);
 }
